3c16bee206e7bbd8814cb8df6cc80b00e06accf3
Alexander Babel <alexander.babel@icloud.com>
2019-08-12 19:51:31 +0200

    Small adjustment

--- helper/storage.js
+++ helper/storage.js
@@ -2,12 +2,12 @@ import { readFileSync, writeFileSync } from 'fs';
 
 export function load() {
   try {
-    return JSON.parse(readFileSync('persitentData.json').toString()) || {};
+    return JSON.parse(readFileSync('persitent/data.json').toString()) || {};
     // eslint-disable-next-line no-empty
   } catch (err) {}
   return {};
 }
 
 export function save(data) {
-  return writeFileSync('persitentData.json', JSON.stringify(data, null, 2));
+  return writeFileSync('persitent/data.json', JSON.stringify(data, null, 2));
 }
