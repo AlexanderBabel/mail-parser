f5a2a7cb2433fcb2a540ddf40b6dab498dbaa05a
Alexander Babel <alexander.babel@icloud.com>
2021-06-24 01:07:28 +0200

    fix(main): increase size limit to 50mb

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
