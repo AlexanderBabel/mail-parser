9a090e3838d7bb075d820b64b50f57de32589b1b
Alexander Babel <alexander.babel@icloud.com>
2021-10-16 03:29:00 +0200

    fix(CardPaymentsModule): enable cron

--- src/card-payments/card-payments.module.ts
+++ src/card-payments/card-payments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { config } from '../config';
 import { CardPaymentsApiService } from './services/card-payments-api.service';
 import { CardPaymentsController } from './card-payments.controller';
 import { PrismaService } from './services/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [config] })],
+  imports: [ConfigModule.forRoot({ load: [config] }), ScheduleModule.forRoot()],
   controllers: [CardPaymentsController],
   providers: [CardPaymentsApiService, PrismaService],
 })
