d89bd62ebdca3545360700453d47d33ebf7637a7
Alex Babel <alexander.babel@icloud.com>
2022-06-10 12:08:24 +0200

    fix: disable card payments module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CardPaymentsModule } from './card-payments/card-payments.module';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [HealthCheckModule, MailModule, CardPaymentsModule],
+  imports: [HealthCheckModule, MailModule],
   controllers: [],
   providers: [],
 })
